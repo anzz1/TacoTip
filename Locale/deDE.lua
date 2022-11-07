40d0551ea6a9503fc6c33721b176910e45ccdef4
shakimas <117395266+shakimas@users.noreply.github.com>
2022-11-07 13:27:09 +0100

    update deDE
    in progress...

--- Locale/deDE.lua
+++ Locale/deDE.lua
@@ -6,5 +6,64 @@
 
 --]]
 
-if (GetLocale() ~= "deDE") then return end
+if (GetLocale() ~= "deDE") then return 
+
+    ["Player"] = "Spieler",
+    ["Pet"] = "Tier",
+    ["Target"] = "Ziel",
+    ["None"] = "Nichts",
+    ["Self"] = "Ziel",
+    ["You"] = "Du",
+    ["Talents"] = "Talente",
+    ["Style"] = "Style",
+    ["Guild"] = "Gilde",
+    ["Rank"] = "Rang",
+    ["Level"] = "Level",
+    ["Item Level"] = "Item Level",
+    ["Left-Click"] = "Links-Click",
+    ["Middle-Click"] = "Mittel-Click",
+    ["Right-Click"] = "Rechts-Click",
+    ["Drag to Move"] = "Zum Bewegen ziehen",
+    ["Change Anchor"] = "Ändere Anker",
+    ["Save Position"] = "Position speichern",
+    [" the Kingslayer"] = " der Königsmörder",
+    ["Undead"] = "Untot",
+    ["Unit Tooltips"] = "Einheiten-Tooltips",
+    ["Class Color"] = "Klassenfarben",
+    ["Color class names in tooltips"] = "Klassenfarben in Tooltips",
+    ["Title"] = "Titel",
+    ["Show player's title in tooltips"] = "Zeige Spielertitel in den Tooltips",
+    ["Guild Name"] = "Gildenname",
+    ["Show guild name in tooltips"] = "Zeige Gildennamen in den Tooltips",
+    ["Guild Rank"] = "Gildenrang",
+    ["Show guild rank in tooltips"] = "Zeige Gildenrang in den Tooltips",
+    ["Show talents and specialization in tooltips"] = "Zeige Talente und Spezialisierungen  in den Tooltips",
+    ["Show player's GearScore in tooltips"] = "Zeige den Gearscore des Spielers in den Tooltips",
+    ["Show player's PawnScore in tooltips (may affect performance)"] = "Zeige den PawnScore in den Tooltips (kann zu Performanceverlust führen)",
+    ["requires Pawn"] = "benötigt Pawn",
+    ["Show unit's target in tooltips"] = "Zeige das Ziel des Spielers in den Tooltips",
+    ["Faction Icon"] = "Fraktionsicon",
+    ["Show player's faction icon (Horde/Alliance) in tooltips"] = "Zeige das Fraktionsicon (Horde/Allianz) in den Tooltips",
+    ["PVP Icon"] = "PVP Icon",
+    ["Show player's pvp flag status as icon instead of text"] = "Zeige den PVP-Status als Icon anstatt den Text",
+    ["Health Bar"] = "Gesundheitsleiste",
+    ["Show unit's health bar under tooltip"] = "Zeige die Gesundheitsleiste des Spielers unter dem Tooltip",
+    ["Power Bar"] = "Energieleiste",
+    ["Show unit's power bar under tooltip"] = "Zeige die Energieleiste des Spielers unter dem Tooltip",
+    ["Character Frame"] = "Charakterfenster",
+    ["Show GearScore in character frame"] = "Zeige den GearScore im Charakterfenster",
+    ["Average iLvl"] = "Durchschnittliches iLvl",
+    ["Show Average Item Level in character frame"] = "Zeige duchschnittliches iLvl im Charakterfenster",
+    ["Lock Position"] = "Sichere Position",
+    ["Lock GearScore and Average Item Level positions in character frame"] = "Sichere die Position des GearScore und des durchschnittliche iLvl im Charakterfenster",
+    ["Extra"] = "Extra",
+    ["Show Item Level"] = "Zeige das Itemlevel an",
+    ["Display item level in the tooltip for certain items."] = "Zeige das Itemlevel im Tooltip für bestimmte Items an.",
+    ["Show Item GearScore"] = "Zeige Item GearScore an",
+    ["Show GearScore in item tooltips"] = "Zeige GearScore in Item-Tooltips an"
+
+
+
+
+end
 
